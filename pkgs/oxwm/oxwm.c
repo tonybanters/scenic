d0f980210bd56417602fa82d20b23e15edf11321
Sigmanificient <edhyjox@gmail.com>
2026-05-10 22:10:34 +0200

    set gplv2 license

--- pkgs/oxwm/oxwm.c
+++ pkgs/oxwm/oxwm.c
@@ -1,3 +1,18 @@
+/* Copyright (C) 2026 tonybanters (tony@tonybtw.com)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   The GNU General Public License is contained in the file LICENSE.
+*/
+
 #include "oxwm.h"
 #include "../glibc/glibc.h"
 #include "../lua/lua.h"
@@ -10,7 +25,7 @@ static const pkg *const oxwm_deps[] = {
 const pkg pkgs_oxwm = {
     .name        = "oxwm",
     .version     = "0.11.4",
-    .src         = "https://github.com/tonybanters/oxwm/archive/refs/tags/v0.11.4.tar.gz",
+    .src         = "https://github.com/tonybanters (tony@tonybtw.com)/oxwm/archive/refs/tags/v0.11.4.tar.gz",
     .sha256      = "0000000000000000000000000000000000000000000000000000000000000000",
     .deps        = { .data = oxwm_deps, .len = sizeof(oxwm_deps) / sizeof(oxwm_deps[0]) },
     .build_flags = "",
